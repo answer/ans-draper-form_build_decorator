be1e93c7b5e2cb947b7526b75d08efdf207edf65
sakai shunsuke <sakai@ans-web.co.jp>
2013-01-05 17:49:09 +0900

    add initial module

--- lib/ans-draper-form_build_decorator.rb
+++ lib/ans-draper-form_build_decorator.rb
@@ -3,7 +3,55 @@ require "ans-draper-form_build_decorator/version"
 module Ans
   module Draper
     module FormBuildDecorator
-      # Your code goes here...
+      def self.included(decorator)
+        decorator.send :extend, ClassMethods
+        decorator.send :include, InstanceMethods
+
+        decorator.class_eval do
+          attr_accessor :form_builder
+          alias_method :f, :form_builder
+        end
+      end
+
+      module ClassMethods
+        def bind(form_builder)
+          new(form_builder.object).tap{|d| d.form_builder = form_builder}
+        end
+        def decorate_form(field,method,opts)
+          define_method field do
+            case method
+            when :form
+              send "_#{method}", field, opts
+            end
+          end
+        end
+      end
+
+      module InstanceMethods
+        private
+
+        def _form(field,opts)
+          case type = (opts[:type] || :string).to_sym
+          when :boolean
+            f.label field, raw("#{h.h _label(field,opts)} #{f.check_box field, class: opts[:class]}"), class: "checkbox"
+          else
+            f.send _method(type), field, placeholder: _label(field,opts), class: opts[:class]
+          end
+        end
+        def _method(type)
+          case type
+          when :password
+            :password_field
+          when :text
+            :text_area
+          else
+            :text_field
+          end
+        end
+        def _label(field,opts)
+          opts[:label] || model.class.human_attribute_name(field)
+        end
+      end
     end
   end
 end
